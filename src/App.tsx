e1f1e0c6f353e6891b8b155d196b4c14a883b2e6
nishantnandeshwar <nishantnandeshwar8@gmail.com>
2024-12-21 00:00:07 +0530

    header design UI - done

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import Header from "./components/common/Header"
 import SideBar from "./components/common/SideBar"
 import LandingScreen from "./screens/homeSceen/LandingScreen"
-
+ import './App.css'
 const App = () => {
 
   return (
